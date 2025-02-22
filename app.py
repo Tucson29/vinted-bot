3f36658a2e8fc2c1d8e4a8a47080058d58d7fefd
Tucson29 <bbaloghbbenjamin@gmail.com>
2025-02-22 21:16:42 +0100

    just trying... Changed the WEBHOOK_URL (added the second parameter)

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
-WEBHOOK_URL = os.getenv('DISCORD_WEBHOOK_URL')
+WEBHOOK_URL = os.getenv('DISCORD_WEBHOOK_URL', "https://discord.com/api/webhooks/1342925652101304330/xxKBNz7qrURFTwlx27zeia4j7yviWRrLIx8p245c5s3jvTE2Tmblh1_fA74Fcq8oNMnK")
 VINTED_API_URL = "https://www.vinted.com/api/v2/catalog/items?order=newest_first&time=1740254213&search_text=ralph%20lauren&page=1"
 
 HEADERS = {
