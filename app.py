b9963bf7dc70b73f7156bf8f7f586c25f2c16701
Tucson29 <140443109+Tucson29@users.noreply.github.com>
2025-03-08 14:13:39 +0100

    repeat 10 times

--- app.py
+++ app.py
@@ -180,6 +180,6 @@ def send_to_discord(item):
         print(f"❌ Failed to send to Discord. Status: {response.status_code}, Response: {response.text}")
 
 if __name__ == "__main__":
-    for i in range(5):
+    for i in range(10):
         scrape_vinted()
         time.sleep(60)
